a63738f668e30d207b8acbd376b4f0312eb841e9
Ana <asya1551@gmail.com>
2024-06-06 15:09:30 -0400

    update data provider

--- src/test/java/com/lumatest/data/TestData.java
+++ src/test/java/com/lumatest/data/TestData.java
@@ -12,6 +12,22 @@ public class TestData {
     public static final String WHATS_NEW_MENU_URL = BASE_URL + "/what-is-new.html";
     public static final String WHATS_NEW_MENU_TITLE = "What's New";
 
+    public static final By WOMEN_MENU = By.xpath("//nav//span[text()='Women']");
+    public static final String WOMEN_URL = BASE_URL + "/women.html";
+    public static final String WOMEN_TITLE = "Women";
+
+    public static final By MEN_MENU = By.xpath("//nav//span[text()='Men']");
+    public static final String MEN_URL = BASE_URL + "/men.html";
+    public static final String MEN_TITLE = "Men";
+
+    public static final By GEAR_MENU = By.xpath("//nav//span[text()='Gear']");
+    public static final String GEAR_URL = BASE_URL + "/gear.html";
+    public static final String GEAR_TITLE = "Gear";
+
+    public static final By TRAINING_MENU = By.xpath("//nav//span[text()='Training']");
+    public static final String TRAINING_URL = BASE_URL + "/training.html";
+    public static final String TRAINING_TITLE = "Training";
+
     public static final By SALE_MENU = By.xpath("//nav//span[text()='Sale']");
     public static final String SALE_URL = BASE_URL + "/sale.html";
     public static final String SALE_TITLE = "Sale";
@@ -20,6 +36,10 @@ public class TestData {
     public static Object[][] getNavMenuData() {
         return new Object[][] {
                 {BASE_URL, WHATS_NEW_MENU, WHATS_NEW_MENU_URL, WHATS_NEW_MENU_TITLE},
+                {BASE_URL, WOMEN_MENU, WOMEN_URL, WOMEN_TITLE},
+                {BASE_URL, MEN_MENU, MEN_URL, MEN_TITLE},
+                {BASE_URL, GEAR_MENU, GEAR_URL, GEAR_TITLE},
+                {BASE_URL, TRAINING_MENU, TRAINING_URL, TRAINING_TITLE},
                 {BASE_URL, SALE_MENU, SALE_URL, SALE_TITLE}
         };
     }
